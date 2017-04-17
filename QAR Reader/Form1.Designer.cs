fc043deb5cb6608f210b4f086466a6a7045038f9
Sasha Kosyanchuk <dedanaan00@gmail.com>
2017-04-17 13:28:20 +0300

    QAR reader

---
+++ QAR Reader/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace QAR_Reader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.openFile = new System.Windows.Forms.ToolStripButton();
+            this.export = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.clusterNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.startCopyTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flightLength = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.worker = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar});
+            this.statusStrip.Location = new System.Drawing.Point(0, 251);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(597, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile,
+            this.export});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(597, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "toolStrip1";
+            // 
+            // openFile
+            // 
+            this.openFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.openFile.Image = ((System.Drawing.Image)(resources.GetObject("openFile.Image")));
+            this.openFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(54, 22);
+            this.openFile.Text = "Open file";
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // export
+            // 
+            this.export.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.export.Image = ((System.Drawing.Image)(resources.GetObject("export.Image")));
+            this.export.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.export.Name = "export";
+            this.export.Size = new System.Drawing.Size(43, 22);
+            this.export.Text = "Export";
+            this.export.Click += new System.EventHandler(this.export_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clusterNum,
+            this.startCopyTime,
+            this.flightLength});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 25);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(597, 226);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // clusterNum
+            // 
+            this.clusterNum.HeaderText = "Cluster Num";
+            this.clusterNum.Name = "clusterNum";
+            this.clusterNum.ReadOnly = true;
+            // 
+            // startCopyTime
+            // 
+            this.startCopyTime.HeaderText = "Start Copy Time";
+            this.startCopyTime.Name = "startCopyTime";
+            this.startCopyTime.ReadOnly = true;
+            // 
+            // flightLength
+            // 
+            this.flightLength.HeaderText = "Flight Length";
+            this.flightLength.Name = "flightLength";
+            this.flightLength.ReadOnly = true;
+            // 
+            // worker
+            // 
+            this.worker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.worker_DoWork);
+            this.worker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.worker_ProgressChanged);
+            this.worker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.worker_RunWorkerCompleted);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 273);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.statusStrip);
+            this.Name = "FormMain";
+            this.Text = "QAR Viewer";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton openFile;
+        private System.Windows.Forms.ToolStripButton export;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.ComponentModel.BackgroundWorker worker;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clusterNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startCopyTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn flightLength;
+    }
+}
+
